35ed17ca6cf7731d9d85b425b9775d10795bc5ff
budougumi0617 <budougumi0617@gmail.com>
2013-07-18 20:54:53 +0900

    ex01_16までリファクタリング

--- JPL/ch01/ex01_16/src/ex01_16/MyUtilities.java
+++ JPL/ch01/ex01_16/src/ex01_16/MyUtilities.java
@@ -9,40 +9,70 @@
  */
 package ex01_16;
 
+import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 
 class BadDataSetException extends Exception {
-	public String file;
-    public IOException e;
+	public String fileInfomation;
+	public IOException exceptionInfomation;
 
+	public BadDataSetException(String filename, IOException e) {
+		fileInfomation = filename;
+		exceptionInfomation = e;
+
+	}
+
+	@Override
+	/*
+	 * (非 Javadoc)
+	 * 
+	 * @see java.lang.Throwable#getStackTrace()
+	 */
+	public StackTraceElement[] getStackTrace() {
+
+		return exceptionInfomation.getStackTrace();
+	}
 }
 
-
 public class MyUtilities {
-	public double[]  getDataSet (String setName)
-			throws BadDataSetException
-	{
+	static final int readLimit = 100;
+
+	public double[] getDataSet(String setName) throws BadDataSetException {
 		String file = setName + ".dset";
 		FileInputStream in = null;
 		try {
 			in = new FileInputStream(file);
 			return readDataSet(in);
 		} catch (IOException e) {
-			throw new BadDataSetException();
+			throw new BadDataSetException(file, e);
 		} finally {
 			try {
 				if (in != null)
 					in.close();
-			} catch (IOException e){
-				; //無視
+			} catch (IOException e) {
+				e.getStackTrace();
 			}
 		}
 	}
 
-	public double[] readDataSet(FileInputStream in)
-	{
-		double[] x = new double[2];
-		return x;
+	public double[] readDataSet(FileInputStream inputStream) {
+		double[] arrayDouble = new double[readLimit];
+		BufferedInputStream bufferedStream = new BufferedInputStream(
+				inputStream);
+		try {
+			for (int i = 0; (bufferedStream.read() != -1)
+					&& i < arrayDouble.length; i++) {
+				try {
+					arrayDouble[i] = bufferedStream.read();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return arrayDouble;
 	}
 }

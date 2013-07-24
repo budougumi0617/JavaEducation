344498364dd96aa1cb720a14f35106bd47ba1193
budougumi0617 <budougumi0617@gmail.com>
2013-07-24 09:26:06 +0900

    リファクタリング

--- GUI/1_1/src/oneOne/Clock.java
+++ GUI/1_1/src/oneOne/Clock.java
@@ -20,28 +20,25 @@ import java.awt.event.ActionListener;
 import java.util.Calendar;
 
 public class Clock extends Frame implements ActionListener, Runnable {
-	/**
-	 * Windowを作成し、デジタル時計を描画するJavaアプリケーション フリー素材　http://digilog.client.jp/
-	 */
 	private static final long serialVersionUID = 1L;
 	static String hourValue; // 時を入れる変数を宣言
 	static String minutsValue; // 分を入れる変数を宣言
 	static String secondValue; // 秒を入れる変数を宣言
 	private int fontSize = 100;
 	FontMetrics fo;
-	private int strWidth = 100;
-	private int strHeight = 100;
+	private int clockDataWidth = 100;
+	private int clockDataHeight = 100;
 	private Color fontColor = Color.white;
 	private Color backColor = Color.black;
 	private String fontName = "Serif";
 
-	public void update(Graphics g) {
-		paint(g);
+	public void update(Graphics graohInstance) {
+		paint(graohInstance);
 	}
 
 	Clock() {
 		this.setTitle("Shimizu's Clock");
-		this.setSize(strWidth, strHeight);
+		this.setSize(clockDataWidth, clockDataHeight);
 
 	}
 
@@ -54,7 +51,6 @@ public class Clock extends Frame implements ActionListener, Runnable {
 
 		threadInstance.start(); // スレッドスタート
 	}
-
 	public void run() {
 		while (true) {
 			hourValue = String.format("%1$02d",
@@ -64,7 +60,7 @@ public class Clock extends Frame implements ActionListener, Runnable {
 			secondValue = String.format("%1$02d",
 					Calendar.getInstance().get(Calendar.SECOND)); // 秒を代入
 
-			this.setSize((int) (strWidth * 1.5), (int) (strHeight * 1.5));
+			this.setSize((int) (clockDataWidth * 1.5), (int) (clockDataHeight * 1.5));
 			repaint();
 
 			try {
@@ -74,21 +70,22 @@ public class Clock extends Frame implements ActionListener, Runnable {
 		}
 	}
 
+	@Override
 	public void paint(Graphics g) {
-		Image offImage = createImage((int) (strWidth * 1.5),
-				(int) (strHeight * 1.5));
+		Image offImage = createImage((int) (clockDataWidth * 1.5),
+				(int) (clockDataHeight * 1.5));
 		Graphics offGraph = offImage.getGraphics();
 		offGraph.setFont(new Font(fontName, Font.PLAIN, this.fontSize));
 		offGraph.setColor(this.fontColor);
 		setBackground(this.backColor);
 		offGraph.drawString(hourValue + ":" + minutsValue + ":" + secondValue,
-				strWidth / 4, (int) (strHeight * 1.1));
+				clockDataWidth / 4, (int) (clockDataHeight * 1.1));
 		g.drawImage(offImage, 0, 0, this);
 		fo = offGraph.getFontMetrics();
-		strWidth = fo.stringWidth("00:00:00");
-		strHeight = fo.getHeight();
+		clockDataWidth = fo.stringWidth("00:00:00");
+		clockDataHeight = fo.getHeight();
 	}
-
+	
 	public void actionPerformed(ActionEvent e) {
 		System.out.println(e.getActionCommand());
 	}

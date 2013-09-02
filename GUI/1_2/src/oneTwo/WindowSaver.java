f18b3a08124d32112c194a6e63cd86e61dad2c7c
budougumi0617 <budougumi0617@gmail.com>
2013-09-03 08:18:49 +0900

    クラス名変更

---
+++ GUI/1_2/src/oneTwo/WindowSaver.java
@@ -0,0 +1,37 @@
+/*
+ * @file
+ * @par File Name:
+ * WindowsServer.java
+ * @author Yoichiro Shimizu
+ * @date Created on 2013/09/02
+ * @par Copyright:
+ * Ricoh IT Solutions, LTD.
+ */
+
+
+package oneTwo;
+
+import java.awt.AWTEvent;
+import java.awt.event.AWTEventListener;
+
+/**
+ * JAVA SWING HACKS P205 #34
+ * @author Yoichiro Shimizu
+ *
+ */
+public class WindowSaver implements AWTEventListener {
+	private static WindowSaver saver;
+	
+
+	@Override
+	public void eventDispatched(AWTEvent event) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}

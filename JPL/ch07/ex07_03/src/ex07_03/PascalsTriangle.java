d813f6231e5070cff1b9e71f0f6df848840bfcbb
Yoichiro Shimizu <budougumi0617@gmail.com>
2013-09-18 01:10:50 +0900

    formed balance

--- JPL/ch07/ex07_03/src/ex07_03/PascalsTriangle.java
+++ JPL/ch07/ex07_03/src/ex07_03/PascalsTriangle.java
@@ -10,36 +10,50 @@
 
 package ex07_03;
 
-
+/**
+ * 
+ * @author budou-gumi
+ * 
+ */
 public class PascalsTriangle {
 	private int pascal[][];
+
 	public PascalsTriangle(int num) {
 		pascal = new int[num][];
-		for(int i=0,j=0;i<pascal.length;i++){
-			pascal[i] = new int[i+1];
-			pascal[i][j]=1;
-			pascal[i][i]=1;
+		for (int i = 0, j = 0; i < pascal.length; i++) {
+			pascal[i] = new int[i + 1];
+			pascal[i][j] = 1;
+			pascal[i][i] = 1;
 		}
 		calc();
 	}
-	public int[][] calc(){
-		for(int j=2;j<pascal.length;j++)
-			for(int i=1;i<pascal[j].length-1;i++)
-				pascal[j][i]=pascal[j-1][i-1]+pascal[j-1][i];
+
+	public int[][] calc() {
+		for (int j = 2; j < pascal.length; j++)
+			for (int i = 1; i < pascal[j].length - 1; i++) {
+				pascal[j][i] = pascal[j - 1][i - 1] + pascal[j - 1][i];
+			}
 		return pascal;
 	}
-	public void show(){
-		for (int[] array : pascal) {
-			for (int i : array)
-				System.out.printf(" %d ", i);
+
+	public void show() {
+		for (int j = 0; j < pascal.length; j++) {
+			int[] array = pascal[j];
+			for (int i = 0; i < pascal.length - j; i++) {
+				System.out.printf("   ");
+			}
+			for (int i : array) {
+				System.out.printf("%4d ", i);
+			}
+
 			System.out.println();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		PascalsTriangle pt = new PascalsTriangle(12);
 		pt.show();
-		pt = new PascalsTriangle(5);
+		pt = new PascalsTriangle(20);
 		pt.show();
 	}
 }

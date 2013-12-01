b9e332d9fa29b6c9a220d771c43a72b4f4bd3920
Yoichiro Shimizu <Yoichiro.Shimizu@jrits.ricoh.co.jp>
2013-12-02 08:06:40 +0900

    impl ex16_04, ex16_05

--- JPL/ch16/ex16_04/src/ex16_04/PrintAllAnnotation.java
+++ JPL/ch16/ex16_04/src/ex16_04/PrintAllAnnotation.java
@@ -8,9 +8,25 @@
  * Ricoh IT Solutions, LTD.
  */
 
-
 package ex16_04;
 
+import java.lang.annotation.Annotation;
+@Deprecated
 public class PrintAllAnnotation {
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			args = new String[] { "ex16_04.PrintAllAnnotation","test" };
+		}
+		
+		try {
+			Class<?> classObject = Class.forName(args[0]);
+			for(Annotation annotation : classObject.getAnnotations()){
+				System.out.println(annotation);
+			}
+		}catch(ClassNotFoundException e){
+			System.out.println("unknown class: " + args[0]);
+		}
+
+	}
 
 }
--- JPL/ch16/ex16_05/src/ex16_05/ClassContents.java
+++ JPL/ch16/ex16_05/src/ex16_05/ClassContents.java
@@ -10,6 +10,101 @@
 
 package ex16_05;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ClassContents {
+	private static int depth;
+
+	private static Map<String, Void> methods = new HashMap<String, Void>();
+
+	static void printMemberts(Member[] member) {
+		for (Member m : member) {
+			if (m.getDeclaringClass() == Object.class) {
+				continue;
+			}
+
+			StringBuilder annotationStrinbBuilder = new StringBuilder();
+
+			if (m instanceof Method) {
+				Method method = (Method) m;
+
+				StringBuilder stringBuilder = new StringBuilder();
+				stringBuilder.append(method.getName());
+
+				for (Class<?> c : method.getParameterTypes()) {
+					stringBuilder.append(":" + c.getName());
+				}
+				if (methods.containsKey(stringBuilder.toString())) {
+					continue;
+				} else {
+					methods.put(stringBuilder.toString(), null);
+				}
+
+				for (Annotation annotation : method.getAnnotations()) {
+					annotationStrinbBuilder.append(" @");
+					annotationStrinbBuilder.append(annotation.annotationType()
+							.getSimpleName());
+
+				}
+			} else if (m instanceof Field) {
+				Field field = (Field) m;
+				for (Annotation annotation : field.getAnnotations()) {
+					annotationStrinbBuilder.append(" @");
+					annotationStrinbBuilder.append(annotation.annotationType()
+							.getSimpleName());
+				}
+			}
+
+			String decl = m.toString() + annotationStrinbBuilder.toString();
+			for (int i = 0; i < depth + 1; i++) {
+				System.out.print(" ");
+			}
+			System.out.println(strip(decl, "jaba.lang"));
+		}
+	}
+
+	private static String strip(String decl, String pattern) {
+		return decl.replaceFirst(pattern, "");
+	}
+
+	private static void contentsValue(Class<?> classObject)
+			throws ClassNotFoundException {
+		if (classObject == null) {
+			throw new ClassNotFoundException();
+		}
+		for (int i = 0; i < depth; i++) {
+			System.out.print(" ");
+		}
+		System.out.println(classObject);
+		printMemberts(classObject.getFields());
+		printMemberts(classObject.getConstructors());
+		printMemberts(classObject.getMethods());
+
+		depth++;
+		if (classObject.getGenericSuperclass() != Object.class) {
+			if (classObject.getGenericSuperclass() instanceof Class<?>) {
+				contentsValue((Class<?>) classObject.getGenericSuperclass());
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		depth = 0;
+		if (args.length == 0) {
+			args = new String[] { "java.lang.String" };
+		}
+		try {
+			Class<?> c = Class.forName(args[0]);
+			contentsValue(c);
+		} catch (ClassNotFoundException e) {
+			System.out.println("unknown class: " + args[0]);
+		}
+	}
 
 }

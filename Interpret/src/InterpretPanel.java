e92e5d8fa37a31c313fc0916881e698455bdc4df
Yoichiro Shimizu <Yoichiro.Shimizu@jrits.ricoh.co.jp>
2013-12-05 20:00:22 +0900

    modify Interpret

--- Interpret/src/InterpretPanel.java
+++ Interpret/src/InterpretPanel.java
@@ -1,7 +1,10 @@
+import java.awt.Color;
+import java.awt.Font;
+
 import javax.swing.BoxLayout;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
@@ -23,6 +26,7 @@ public class InterpretPanel extends JFrame {
 	 * @wbp.nonvisual location=108,-4
 	 */
 	private final JLabel label = DefaultComponentFactory.getInstance().createTitle("New JGoodies title");
+	private JTextField txtTest;
 	public static void main(String[] args) {
 		InterpretPanel frame = new InterpretPanel();
 
@@ -33,22 +37,73 @@ public class InterpretPanel extends JFrame {
 	}
 
 	InterpretPanel() {
+		setFont(new Font("Inconsolata", Font.PLAIN, 12));
+		setForeground(new Color(102, 102, 102));
+		setTitle("Interpret");
+		setBackground(new Color(204, 153, 255));
 		getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.X_AXIS));
 		
 		JPanel panel = new JPanel();
+		panel.setBackground(new Color(204, 153, 255));
 		getContentPane().add(panel);
-		JTabbedPane tabbedpane = new JTabbedPane();
+		panel.setLayout(null);
+		
+		txtTest = new JTextField();
+		txtTest.setBackground(new Color(204, 204, 255));
+		txtTest.setText("type Class name");
+		txtTest.setBounds(12, 28, 153, 19);
+		panel.add(txtTest);
+		txtTest.setColumns(10);
+		
+		JTabbedPane instansePane = new JTabbedPane(JTabbedPane.TOP);
+		instansePane.setBounds(12, 80, 167, 217);
+		panel.add(instansePane);
+		
+		JList objectList = new JList();
+		instansePane.addTab("Object", null, objectList, null);
+		
+		JList arrayList = new JList();
+		instansePane.addTab("Array", null, arrayList, null);
+		JTabbedPane classMamberpane = new JTabbedPane();
+		classMamberpane.setBackground(new Color(204, 153, 255));
 
-		JPanel tabPanel1 = new JPanel();
-		tabPanel1.add(new JButton("button1"));
+		JPanel cstTabPanel = new JPanel();
+		cstTabPanel.setBackground(new Color(204, 153, 255));
 
-		JPanel tabPanel2 = new JPanel();
-		tabPanel2.add(new JLabel("Name:"));
-		tabPanel2.add(new JTextField("", 10));
+		JPanel methodTabPanel = new JPanel();
+		methodTabPanel.setBackground(new Color(204, 153, 255));
+		methodTabPanel.setLayout(null);
+		JLabel label_1 = new JLabel("Name:");
+		label_1.setBounds(12, 8, 31, 13);
+		methodTabPanel.add(label_1);
+		JTextField textField = new JTextField("", 10);
+		textField.setBounds(55, 5, 96, 19);
+		methodTabPanel.add(textField);
 
-		tabbedpane.addTab("tab1", tabPanel1);
-		tabbedpane.addTab("tab2", tabPanel2);
+		JPanel fieldTabPanel = new JPanel();
+		fieldTabPanel.setBackground(new Color(204, 153, 255));
+		fieldTabPanel.add(new JLabel("Name:"));
+		fieldTabPanel.add(new JTextField("", 10));
+		
+		
+		
+		classMamberpane.addTab("Cst", cstTabPanel);
+		cstTabPanel.setLayout(null);
+		
+		JList list = new JList();
+		list.setBounds(165, 5, 0, 0);
+		cstTabPanel.add(list);
+		
+		JList list_1 = new JList();
+		list_1.setBounds(12, 10, 307, 150);
+		cstTabPanel.add(list_1);
+		classMamberpane.addTab("Field", fieldTabPanel);
+		classMamberpane.addTab("Method", methodTabPanel);
+		
 
-		getContentPane().add(tabbedpane);
+		getContentPane().add(classMamberpane);
+		
+		JTabbedPane listDisp = new JTabbedPane(JTabbedPane.TOP);
+		classMamberpane.addTab("ListDisp", null, listDisp, null);
 	}
 }

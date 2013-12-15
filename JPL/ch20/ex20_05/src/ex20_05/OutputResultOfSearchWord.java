fd11657b9b501aa7101c94f4a816ffc3d90b986e
Yoichiro Shimizu <Yoichiro.Shimizu@jrits.ricoh.co.jp>
2013-12-16 08:13:15 +0900

    add ex20_05

---
+++ JPL/ch20/ex20_05/src/ex20_05/OutputResultOfSearchWord.java
@@ -0,0 +1,27 @@
+/*
+ * @file
+ * @par File Name:
+ * OutputResultOfSearchWord.java
+ * @author Yoichiro Shimizu
+ * @date Created on 2013/12/15
+ * @par Copyright:
+ * Ricoh IT Solutions, LTD.
+ */
+package ex20_05;
+
+/**
+ * @author Yoichiro Shimizu
+ * 指定されたファイルを読み込んで、指定された単語を検索するプログラムを作成しなさい。
+ * 単語が発見された全ての行を、行の前に行番号をつけて表示しなさい。
+ */
+public class OutputResultOfSearchWord {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}

2f7c3a027afe42f9f5ea57136be659300d1254e9
Yoichiro Shimizu <budougumi0617@gmail.com>
2013-12-18 04:45:44 +0900

    add

--- JPL/ch16/ex16_11/src/ex16_11/Game.java
+++ JPL/ch16/ex16_11/src/ex16_11/Game.java
@@ -21,40 +21,47 @@ public class Game {
 	static final int SIZE = 9;
 	static final int FREE = 0;
 	static final int MARU = 1;
-	static final int BATU = (-1);
+	static final int BATU = 5;
 	static final int LINE = 8;
 	static final int MARU_WIN = 1;
-	static final int BATU_WIN = (-1);
+	static final int BATU_WIN = 5;
 	static final int DRAW = 0;
 	static final int MAX_VALUE = 2;
 	static final int MIN_VALUE = (-2);
 
 	/* 盤面 */
-	char board[];
-	static final char line[][];
+	static char board[];
+	char[][] line = { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 }, { 0, 3, 6 },
+			{ 1, 4, 7 }, { 2, 5, 8 }, { 0, 4, 8 }, { 2, 4, 6 }, };
 
-	public Game(){
+	public Game() {
 		board = new char[SIZE];
-		line[LINE][3] = new char[LINE][3]{
-				  0, 1, 2,   3, 4, 5,   6, 7, 8,
-				  0, 3, 6,   1, 4, 7,   2, 5, 8,
-				  0, 4, 8,   2, 4, 6,
-				};
+
 	}
 
 	public static void main(String[] args) {
-		String name; // Class name
-		while ((name = getNextPlayer()) != null) {
-			try {
-				PlayerLoader loader = new PlayerLoader();
-			} catch (Exception e) {
-				reportException(name, e);
-			}
+		/*
+		 * String name; // Class name while ((name = getNextPlayer()) != null) {
+		 * try { PlayerLoader loader = new PlayerLoader(); } catch (Exception e)
+		 * { reportException(name, e); } }
+		 */
+		Game g = new Game();
+		int v, i, j;
+		for (i = 0; i < SIZE; i++) {
+			/* 初期化 */
+		
+			/* 初手 */
+			board[i] = MARU;
+			/* 相手の手番 */
+			v = g.thinkBatu(1);
+			/* 結果 */
+			System.out.printf("初手 %d: 評価値 %d\n", i, v);
 		}
+		return;
 
 	}
 
-	int check_winner() {
+	int checkWinner() {
 		int i;
 		for (i = 0; i < LINE; i++) {
 			int piece = board[line[i][0]];
@@ -64,6 +71,50 @@ public class Game {
 			}
 		}
 		return DRAW;
+	}
+
+	/* 先手 */
+	int thinkMaru(int n) {
+		int i, v, value = MIN_VALUE;
+		for (i = 0; i < SIZE; i++) {
+			if (board[i] != FREE)
+				continue;
+			/* MARU を書く */
+			board[i] = MARU;
+			/* 勝敗の判定 */
+			v = checkWinner();
+			/* 決着していなければ相手の手番へ */
+			if (v == DRAW && n < SIZE - 1)
+				v = thinkBatu(n + 1);
+			/* ミニマックス */
+			if (v > value)
+				value = v;
+			/* 元に戻す */
+			board[i] = FREE;
+		}
+		return value;
+	}
+
+	/* 後手 */
+	int thinkBatu(int n) {
+		int i, v, value = MAX_VALUE;
+		for (i = 0; i < SIZE; i++) {
+			if (board[i] != FREE)
+				continue;
+			/* BATU を書く */
+			board[i] = (char) BATU;
+			/* 勝敗の判定 */
+			v = checkWinner();
+			/* 決着していなければ相手の手番へ */
+			if (v == DRAW && n < SIZE - 1)
+				v = thinkMaru(n + 1);
+			/* ミニマックス */
+			if (v < value)
+				value = v;
+			/* 元に戻す */
+			board[i] = FREE;
+		}
+		return value;
 	}
 
 	static String getNextPlayer() {

ca6ce2463a5785c3cbb16cf99b2de57d43bf6d6f
budougumi0617 <budougumi0617@gmail.com>
2014-01-13 15:54:14 +0900

    add ex17_03

---
+++ JPL/ch17/ex17_03/src/ex17_03/ResourceImpl.java
@@ -0,0 +1,67 @@
+/**
+ * @file
+ * @par File Name:
+ * ResourceImpl.java
+ * @author budougumi0617
+ * @date Created on 2014/01/13
+ * @par Copyright:
+ * Ricoh IT Solutions, LTD.
+ */
+package ex17_03;
+
+/**
+ * @author budougumi0617
+ * 
+ */
+public class ResourceImpl implements Resource {
+	Object key;
+	boolean needsRelease = false;
+
+	/**
+	 * 
+	 */
+	public ResourceImpl(Object key) {
+		this.key = key;
+		System.out.println("Setting External Resource.");
+	}
+
+	/*
+	 * (非 Javadoc)
+	 * 
+	 * @see ex17_03.Resource#use(java.lang.Object, java.lang.Object[])
+	 */
+	@Override
+	public void use(Object key, Object... args) {
+		if (this.key.equals(key)) {
+			throw new IllegalArgumentException("wrong key");
+		}
+		System.out.println("Use Resource : ");
+		for (Object arg : args) {
+			System.out.println(arg.toString());
+		}
+
+	}
+
+	/*
+	 * (非 Javadoc)
+	 * 
+	 * @see ex17_03.Resource#release()
+	 */
+	@Override
+	public synchronized void release() {
+		if (needsRelease) {
+			needsRelease = false;
+			System.out.println("Release Resource");
+		}
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}

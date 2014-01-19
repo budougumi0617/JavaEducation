032ebc8437fc767321c9ef767f3aebb6db8e9ec5
budougumi0617 <budougumi0617@gmail.com>
2014-01-19 19:17:09 +0900

    bug fix

--- GUI/2_1/src/twoOne/Clock.java
+++ GUI/2_1/src/twoOne/Clock.java
@@ -1,131 +1,69 @@
+
+/*
+ * @file
+ * @par File Name:
+ * Cloack.java
+ * @author Yoichiro Shimizu
+ * @date Created on 2014/01/19
+ * @par Copyright:
+ * Ricoh IT Solutions, LTD.
+ */
 package twoOne;
 
-
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.FontMetrics;
+import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Calendar;
 
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JPanel;
-
-public class Clock extends JFrame implements Runnable {
+public class Clock extends Frame implements ActionListener, Runnable {
 	private static final long serialVersionUID = 1L;
-	static String h; // 時を入れる変数を宣言
-	static String m; // 分を入れる変数を宣言
-	static String s; // 秒を入れる変数を宣言
-	private int strWidth = 300;
-	private int strHeight = 200;
+	static String hourValue; // 時を入れる変数を宣言
+	static String minutsValue; // 分を入れる変数を宣言
+	static String secondValue; // 秒を入れる変数を宣言
 	private int fontSize = 100;
 	FontMetrics fo;
+	private int clockDataWidth = 100;
+	private int clockDataHeight = 100;
 	private Color fontColor = Color.white;
 	private Color backColor = Color.black;
 	private String fontName = "Serif";
-	JPanel mainClock;
 
-	Clock(String title) {
-		super(title);
-		mainClock = new JPanel();
-		this.setSize(strWidth, strHeight);
-		this.setSize(strWidth,strHeight);
+	public void update(Graphics graohInstance) {
+		paint(graohInstance);
 	}
 
-	boolean a = true;
+	Clock() {
+		this.setTitle("Shimizu's Clock");
+		this.setSize(clockDataWidth, clockDataHeight);
+
+	}
 
 	public static void main(String args[]) {
-
+		Clock mainFrame = new Clock();
+		Thread threadInstance = new Thread(mainFrame);
 		// フレーム作成
-		Clock clock = new Clock("Shimizu's Clock");
-		Thread th = new Thread(clock);
-		clock.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		clock.setSize(clock.strWidth, clock.strHeight);
-		clock.setJMenuBar(clock.createMenu());
-		clock.setVisible(true);
+		mainFrame.setVisible(true);
+		mainFrame.addWindowListener(new CloseWindow());
 
-		th.start(); // スレッドスタート
+		threadInstance.start(); // スレッドスタート
 	}
-
-	private JMenuBar createMenu(){
-		JMenuBar menuBar = new JMenuBar();
-		//[Menu]
-		JMenu menuMenu = new JMenu("Menu");
-		//menuMenu.addActionListener(this);
-		menuBar.add(menuMenu);
-		//[Menu]-[Properties]
-		JMenuItem propMenu = new JMenuItem("Prop");
-		//propMenu.addActionListener(this);
-		menuMenu.add(propMenu);
-		// [Font]
-        JMenu fontMenu = new JMenu("Font");
-        //fontMenu.addActionListener(this);
-        menuBar.add(fontMenu);
-        // [Font]-[Serif]
-        JMenuItem fontSerif = new JMenuItem("Serif");
-        fontMenu.add(fontSerif);
-        // [Font]-[----]
-        fontMenu.addSeparator();
-        // [Font]-[SansSerif]
-        JMenuItem fontSansSerif = new JMenuItem("SansSerif");
-        fontMenu.add(fontSansSerif);
-        // [FontSize]
-        JMenu sizeMenu = new JMenu("FontSize");
-        //sizeMenu.addActionListener(this);
-        menuBar.add(sizeMenu);
-        // [FontSize]-[Large]
-        JMenuItem fontLarge = new JMenuItem("Large");
-        sizeMenu.add(fontLarge);
-        // [FontSize]-[Nomal]
-        JMenuItem fontNormal = new JMenuItem("Normal");
-        sizeMenu.add(fontNormal);
-        // [Color]
-        JMenu colorMenu = new JMenu("Color");
-        menuBar.add(colorMenu);
-        // [Color]-[White]
-        JMenuItem colorWhite = new JMenuItem("White");
-        colorMenu.add(colorWhite);
-     // [Color]-[White]
-        JMenuItem colorBlack = new JMenuItem("Black");
-        colorMenu.add(colorBlack);
-     // [BackColor]
-        JMenu backColorMenu = new JMenu("BackColor");
-        menuBar.add(backColorMenu);
-        // [BackColor]-[White]
-        JMenuItem backColorWhite = new JMenuItem("White");
-        backColorMenu.add(backColorWhite);
-     // [BackColor]-[White]
-        JMenuItem backColorBlack = new JMenuItem("Black");
-        backColorMenu.add(backColorBlack);
-/*
-        fontSerif.addActionListener(this);
-        fontSansSerif.addActionListener(this);
-        fontLarge.addActionListener(this);
-        fontNormal.addActionListener(this);
-        colorWhite.addActionListener(this);
-        colorBlack.addActionListener(this);
-        backColorWhite.addActionListener(this);
-        */
-        backColorWhite.setActionCommand("BackWhite");
-        //backColorBlack.addActionListener(this);
-        backColorBlack.setActionCommand("BackBlack");
-
-		return menuBar;
-	}
-
 	public void run() {
-		while (a == true) {
-			h = String.format("%1$02d",
+		while (true) {
+			hourValue = String.format("%1$02d",
 					Calendar.getInstance().get(Calendar.HOUR_OF_DAY)); // 時を代入
-			m = String.format("%1$02d",
+			minutsValue = String.format("%1$02d",
 					Calendar.getInstance().get(Calendar.MINUTE)); // 分を代入
-			s = String.format("%1$02d",
+			secondValue = String.format("%1$02d",
 					Calendar.getInstance().get(Calendar.SECOND)); // 秒を代入
 
-			this.setSize(strWidth, strHeight);
-
+			this.setSize((int) (clockDataWidth * 1.5), (int) (clockDataHeight * 1.5));
 			repaint();
+
 			try {
 				Thread.sleep(500); // スリープ0.5秒
 			} catch (InterruptedException e) {
@@ -133,30 +71,24 @@ public class Clock extends JFrame implements Runnable {
 		}
 	}
 
-	public void paintComponent(Graphics g) {
-		mainClock.paintComponents(g);
-		g.drawString(h + ":" + m + ":" + s, 30, 30);
+	@Override
+	public void paint(Graphics g) {
+		Image offImage = createImage((int) (clockDataWidth * 1.5),
+				(int) (clockDataHeight * 1.5));
+		Graphics offGraph = offImage.getGraphics();
+		offGraph.setFont(new Font(fontName, Font.PLAIN, this.fontSize));
+		offGraph.setColor(this.fontColor);
+		setBackground(this.backColor);
+		offGraph.drawString(hourValue + ":" + minutsValue + ":" + secondValue,
+				clockDataWidth / 4, (int) (clockDataHeight * 1.1));
+		g.drawImage(offImage, 0, 0, this);
+		fo = offGraph.getFontMetrics();
+		clockDataWidth = fo.stringWidth("00:00:00");
+		clockDataHeight = fo.getHeight();
 	}
-	public void setFontName(String obj){
-		this.fontName = obj;
+	
+	public void actionPerformed(ActionEvent e) {
+		System.out.println(e.getActionCommand());
 	}
-	public void setFontSize(int size){
-		this.fontSize = size;
-	}
-	public void setFontColor(String obj){
-		this.fontColor = returnColor(obj);
-	}
-	public void setBackColor(String obj){
-		this.backColor = returnColor(obj);
-	}
-	public Color returnColor(String obj){
-		if(obj.equals("Black"))
-			return Color.black;
-		else if(obj.equals("White"))
-			return Color.white;
-		else if(obj.equals("Blue"))
-			return Color.blue;
-		else
-			return Color.white;
-	}
+
 }
